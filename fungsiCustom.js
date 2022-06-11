e07aa7a671a11a1b0082e0bac5e46472abeed1ca
samuelharold568 <samuelharold327@gmail.com>
2022-06-11 19:55:20 +0700

    membuat fungsi bacaData

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,5 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
-
+const fs = require('fs')
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
 let file2 = "./data2.json";
@@ -18,7 +18,36 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const bacaData = (fnCallback) => {
+  fs.readFile(file1, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    fnCallback = data;
+    console.log(fnCallback)
+  });
+
+  fs.readFile(file2, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    fnCallback = data;
+    console.log(fnCallback)
+  });
+
+  fs.readFile(file3, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    fnCallback = data;
+    console.log(fnCallback)
+  });
+  
+};
+
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
